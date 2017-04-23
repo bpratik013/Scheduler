5eb3182d0213cf6ba986c3ce7b9e503c06ca50b1
bpratik013 <bpratik013@gmail.com>
2017-04-22 23:23:09 -0400

    Initial

---
+++ src/Scheduler.Model/IEntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Model
+{
+    //It will hold the base interface for our entities
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
